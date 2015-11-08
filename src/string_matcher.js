24308956ef8a6e1196a88873a3ef7ded5cb8f2e1
Roger Garcia <roger@redealumni.com>
2015-11-08 01:39:24 -0200

    string matcher working

--- src/string_matcher.js
+++ src/string_matcher.js
@@ -1,92 +1,118 @@
 var StringMatcher = {
-  currentWords: ['arst', 'arpf', 'tsra', 'tsars'],
+  setupWords: function() {
+    window.ALLOWED_COMMANDS = [
+      'punch',
+      'kick',
+      'jump',
+      'squat',
+      'block',
+      'special',
+      'cancel'
+    ];
 
-  possibleMatches: function(partialStr) {
-    // retorna partial matches e full matches
+    window.NUM_COMMANDS = 7;
+    window.currentWords = {};
+    window.currentMatchLevels = {};
+    for(var i = 0; i < window.NUM_COMMANDS; ++i) {
+      StringMatcher.setupWord(window.ALLOWED_COMMANDS[i]);
+    }
+  },
+
+  setupWord: function(index) {
+    window.currentWords[index] = StringMatcher.getRandomWord();;
+    window.currentMatchLevels[index] = 0.0;
+    StringMatcher.updateLabel(index);
+  },
+
+  updateLabels: function() {
+    for(var i = 0; i < window.NUM_COMMANDS; ++i) {
+      StringMatcher.updateLabel(window.ALLOWED_COMMANDS[i]);
+    }
+  },
+
+  updateLabel: function(index) {
+    var wordLabel = document.getElementById('word-' + index),
+      wordMatchLevel = document.getElementById('match-level-' + index);
+
+    wordLabel.innerHTML = window.currentWords[index];
+    wordMatchLevel.innerHTML = window.currentMatchLevels[index];
+  },
+
+  updateMatchLevelsFromInput: function() {
+    var elem = document.getElementById('player-input'),
+      matchLevels = StringMatcher.matchLevels(window.currentWords, elem.value);
+
+    for(var i = 0; i < window.NUM_COMMANDS; ++i) {
+      var idx = window.ALLOWED_COMMANDS[i],
+        matchLevel = matchLevels[idx],
+        matchLevelLabel = document.getElementById('match-level-' + idx);
+
+      window.currentMatchLevels[idx] = matchLevel;
+    }
+
+    // console.log(elem.value);
+    StringMatcher.updateLabels();
+
+  },
+
+  handleUserSubmission: function() {
+    for(var i = 0; i < window.NUM_COMMANDS; ++i) {
+      var idx = window.ALLOWED_COMMANDS[i];
+      if(window.currentMatchLevels[idx] == 1.0) {
+        StringMatcher.commandCompleted(idx);
+        StringMatcher.setupWord(idx);
+      }
+    }
+    document.getElementById('player-input').value = '';
+    StringMatcher.updateMatchLevelsFromInput();
+  },
+
+  commandCompleted: function(command) {
+    console.log('Command completed: ' + command)
+  },
+
+  matchLevels: function(currentWords, partialStr) {
+    // returns match percentage (if 1.0, full match)
     var partialStrLength = partialStr.length,
-      currentWordsLength = StringMatcher.currentWords.length,
+      currentWordsLength = currentWords.length,
       result = {};
 
-
-    for(var i = 0; i < currentWordsLength; ++i) {
-      var candidate = StringMatcher.currentWords[i];
-      if(candidate.indexOf(partialStr) == 0) {
-        result[candidate] = partialStrLength / candidate.length;
+    for(var idx in currentWords) {
+      if(currentWords.hasOwnProperty(idx)) {
+        var candidate = currentWords[idx];
+        if(candidate.indexOf(partialStr) == 0) {
+          result[idx] = partialStrLength / candidate.length;
+        } else {
+          result[idx] = 0.0;
+        }
       }
     }
 
     return result;
   },
 
+  getRandomWord: function(wordLength) {
+    if(!wordLength) {
+      var minLength = 3,
+        maxLength = 8;
+      wordLength = StringMatcher.randInt(minLength, maxLength);
+    }
+
+    var numPossibilities = WordsDatabase[wordLength].length,
+      chosenIndex = StringMatcher.randInt(0, numPossibilities - 1);
+
+    return WordsDatabase[wordLength][chosenIndex];
+  },
+
+  randInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+
+  iterateObject: function(obj, f) {
+    for(var idx in obj) {
+      if(obj.hasOwnProperty(idx)) {
+        f(idx, obj[idx]);
+      }
+    }
+  }
 };
-
-
-
-
-
-// var Network = {
-//   buffer: [],
-//   conn: null,
-//   isHost: false,
-//   lastSentTimestamp: null,
-//   peer: null,
-
-//   client: function(hostKey) {
-//     Network.conn = Network.peer.connect(hostKey, {reliable: true});
-//     Network.conn.on('data', Network.handleReceivedData);
-
-//     console.log('Connected to a new game!');
-//     // Game.init();
-//   },
-
-//   handleReceivedData: function(data) {
-//     console.log('Received data:')
-//     console.log(data)
-//     // magic here
-//   },
-
-//   host: function() {
-//     Network.isHost = true;
-
-//     Network.peer.on('connection', function(conn) {
-//       Network.conn = conn;
-//       Network.conn.on('data', Network.handleReceivedData);
-//       // Game.init();
-//     });
-
-//     console.log('Hosting game!');
-//   },
-
-//   init: function() {
-//     var peer = new Peer({key: '3c8e8hl4a8xgvi'});
-//     Network.peer = peer;
-
-//     peer.on('open', function(id) {
-//       console.log('My peer ID is: ' + id);
-//     });
-//   },
-
-//   appendAction: function(action) {
-//     Network.buffer.push(action)
-//   },
-
-//   _sendData: function(data) {
-//     // console.log('sending data', data);
-//     Network.lastSentTimestamp = Date.now();
-//     Network.conn.send(data);
-//   },
-
-//   sendControls: function(gameState) {
-//     var sendObj = {
-//       controls: Network.buffer
-//     };
-
-//     if(Network.isHost && gameState) {
-//       // if host, include game state information for sync
-//       sendObj.state = gameState;
-//     }
-
-//     Network._sendData(sendObj);
-//     Network.buffer.length = 0;
-//   }
-// };
