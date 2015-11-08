30d1de457840c620e621f21273b0d797ac85db09
Daniel Gracia <daniel.santos@redealumni.com>
2015-11-08 13:50:27 -0200

    Process Jago

---
+++ assets/Jago/juntar.rb
@@ -0,0 +1,33 @@
+files = Dir['*']
+
+groups = files.group_by {|a|  a[/(.*?)\_.*/,1]  }
+
+groups.reject!{ |a,b| a.nil? }
+
+groups.each do |a, b|
+  `mkdir #{a}`
+  `mv #{b.join(' ')} #{a}`
+end
+
+groups.each do |a, b|
+  b.each do |file|
+    c = file[/([0-9]+)_.*\.png/,1]
+    d = file[/[0-9]+_(.*)\.png/,1]
+
+    `mv #{a}/#{file} #{a}/#{c}#{d.rjust(3, '0')}.png`
+  end
+end
+
+groups.each do |a, _|
+  files = Dir["#{a}/*"]
+
+  files.each do |file|
+    `convert #{file} -filter gaussian -resize 320x240 #{file}`
+  end
+end
+
+
+
+
+
+
