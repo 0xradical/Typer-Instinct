d5becc730529f373b04a5075feb93b578f31a221
Thiago Brandão <thiagobrandam@gmail.com>
2015-11-08 15:09:52 -0200

    Improves moves

--- src/Game.ts
+++ src/Game.ts
@@ -193,7 +193,7 @@ class Game {
             this._opponentSprite.animations.add('punch', Phaser.Animation.generateFrameNames('20', 0, 8, '', 2), 23, false, false);
 
             // crouch: 11000
-            this._playerSprite.animations.add('crouch', Phaser.Animation.generateFrameNames('11', 0, 5, '', 3), 23, false, false);
+            this._playerSprite.animations.add('crouch', Phaser.Animation.generateFrameNames('11', 0, 10, '', 3), 23, false, false);
             this._opponentSprite.animations.add('crouch', Phaser.Animation.generateFrameNames('10', 0, 10, '', 3), 23, false, false);
 
             // kick: 35000
@@ -201,7 +201,7 @@ class Game {
             this._opponentSprite.animations.add('kick', Phaser.Animation.generateFrameNames('17', 0, 19, '', 3), 23, false, false);
 
             // block: 192000
-            this._playerSprite.animations.add('block', Phaser.Animation.generateFrameNames('192', 0, 6, '', 3), 10, false, false);
+            this._playerSprite.animations.add('block', Phaser.Animation.generateFrameNames('192', 0, 10, '', 3), 15, false, false);
             this._opponentSprite.animations.add('block', Phaser.Animation.generateFrameNames('59', 0, 6, '', 3), 23, false, false);
 
             // special: 67000
